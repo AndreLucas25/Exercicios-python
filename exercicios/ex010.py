a31594167a70ed6dc9d4a44b61f624337c96179c
AndreLucas <andrelucasmenezessilva@gmail.com>
2025-05-04 12:54:44 -0300

    documentando ex010

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,6 +1,6 @@
 #Ver o valor que o usuário tem na carteira e ver quantos dólares ele pode comprar
 
-print('\033[35mDigite o valor que você tem na carteira\033[m:')
-real = float(input('\033[35mR$\033[m'))
-dolar = real / 6.10
-print(f'Com R${real} você pode comprar US${dolar:.2f}')
+print('\033[35mDigite o valor que você tem na carteira\033[m:') #Titulo
+real = float(input('\033[35mR$\033[m')) #Recebendo o valor
+dolar = real / 6.10 #Fazendo a conversão
+print(f'Com R${real} você pode comprar US${dolar:.2f}') #saida de dados
